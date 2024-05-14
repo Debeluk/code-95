f26dbbe58eb1fcf8c84196b1ee1d6bd79cda926c
Debeluk <popcorn.starinets@gmail.com>
2024-05-14 16:58:41 +0300

    logout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,95 +1,147 @@
 import React from 'react';
 import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  Box,
-  Menu,
-  MenuItem,
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    Box,
+    Menu,
+    MenuItem,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
 } from '@mui/material';
-import {useStore} from "../store/store.js"
+import {useStore} from "../store/store.js";
+import secureLocalStorage from 'react-secure-storage';
+import {useNavigate} from 'react-router-dom';
 
 export const Header = () => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const { user } = useStore(state =>({
-      user: state.currentUser,
-  }));
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [openDialog, setOpenDialog] = React.useState(false);
+    const open = Boolean(anchorEl);
+    const {user, setCurrentUser} = useStore(state => ({
+        user: state.currentUser,
+        setCurrentUser: state.setCurrentUser,
+    }));
+    const navigate = useNavigate();
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    const handleCloseMenu = () => {
+        setAnchorEl(null);
+    };
 
-  return (
-    <AppBar position="sticky" color="default" elevation={3}>
-      <Toolbar disableGutters>
-        <Box
-          sx={{
-            mt: 2,
-            mb: 2,
-            ml: { xs: 2, md: 32 },
-            mr: { xs: 2, md: 32 },
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Typography sx={{ fontSize: { xs: '16px', md: '18px' } }}>ADR Online</Typography>
-          <Button
-            color="inherit"
-            onClick={handleClick}
-            sx={{
-              backgroundColor: 'orange',
-              color: 'white',
-              borderRadius: '24px',
-              padding: '8px 16px',
-              textTransform: 'none',
-              '&:hover': {
-                backgroundColor: 'black',
-              },
-            }}
-          >
-            <Box display="flex" alignItems="center">
-              <Typography variant="subtitle1" mr={1}>{user.name ?? 'Name'}</Typography>
-              <Typography variant="caption">dropdown</Typography>
-            </Box>
-          </Button>
-        </Box>
-      </Toolbar>
-      <Menu
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        PaperProps={{
-          elevation: 3,
-          style: {
-            maxHeight: 200,
-            width: '20ch',
-            marginTop: '20px',
-          },
-        }}
-      >
-        <MenuItem disabled>
-          <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-            Доступ до: 17.12.02
-          </Box>
-        </MenuItem>
-        <MenuItem onClick={handleClose} sx={{ justifyContent: 'center' }}>Вийти</MenuItem>
-      </Menu>
-    </AppBar>
-  );
+    const handleLogout = () => {
+        secureLocalStorage.removeItem('accessToken');
+        secureLocalStorage.removeItem('refreshToken');
+        setCurrentUser(null);
+        navigate('/');
+    };
+
+    const handleOpenDialog = () => {
+        setOpenDialog(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
+
+    const handleConfirmLogout = () => {
+        handleLogout();
+        setOpenDialog(false);
+    };
+
+    return (
+        <AppBar position="sticky" color="default" elevation={3}>
+            <Toolbar disableGutters>
+                <Box
+                    sx={{
+                        mt: 2,
+                        mb: 2,
+                        ml: {xs: 2, md: 32},
+                        mr: {xs: 2, md: 32},
+                        width: '100%',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                    }}
+                >
+                    <Typography sx={{fontSize: {xs: '16px', md: '18px'}}}>ADR Online</Typography>
+                    <Button
+                        color="inherit"
+                        onClick={handleClick}
+                        sx={{
+                            backgroundColor: 'orange',
+                            color: 'white',
+                            borderRadius: '24px',
+                            padding: '8px 16px',
+                            textTransform: 'none',
+                            '&:hover': {
+                                backgroundColor: 'black',
+                            },
+                        }}
+                    >
+                        <Box display="flex" alignItems="center">
+                            <Typography variant="subtitle1" mr={1}>{user.name ?? 'Name'}</Typography>
+                            <Typography variant="caption">dropdown</Typography>
+                        </Box>
+                    </Button>
+                </Box>
+            </Toolbar>
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleCloseMenu}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                PaperProps={{
+                    elevation: 3,
+                    style: {
+                        maxHeight: 200,
+                        width: '20ch',
+                        marginTop: '20px',
+                    },
+                }}
+            >
+                <MenuItem disabled>
+                    <Box sx={{width: '100%', display: 'flex', justifyContent: 'center'}}>
+                        Доступ до: 17.12.02
+                    </Box>
+                </MenuItem>
+                <MenuItem onClick={handleOpenDialog} sx={{justifyContent: 'center'}}>Вийти</MenuItem>
+            </Menu>
+
+            <Dialog
+                open={openDialog}
+                onClose={handleCloseDialog}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Підтвердіть вихід"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Ви впевнені, що хочете вийти?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDialog} color="primary">
+                        Скасувати
+                    </Button>
+                    <Button onClick={handleConfirmLogout} color="primary" autoFocus>
+                        Вийти
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </AppBar>
+    );
 };
